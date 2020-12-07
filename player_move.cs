23a34a6487c7663a5d149f616a1e48da46701009
mekatama <tamatte@hotmail.com>
2020-12-07 22:21:02 +0900

    接地判定add

--- player_move.cs
+++ player_move.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player_move : MonoBehaviour{
-	public float speed;				//速度
+	public float speed;					//速度
 	private Rigidbody2D rb = null;
+	private bool isGround = false;		//接地flag移動用
+	private bool isGroundEnter = false;	//接地時
+	private bool isGroundStay = false;	//接地中
+	private bool isGroundExit = false;	//接地終了時
 
     void Start(){
 		rb = GetComponent<Rigidbody2D>();	//Rigidbody2D取得
@@ -12,8 +16,10 @@ public class player_move : MonoBehaviour{
 
 	//物理演算用
 	void FixedUpdate(){
-		float horizontalKey = Input.GetAxis("Horizontal");
+		//接地判定確認
+		isGround = IsGround();
 
+		float horizontalKey = Input.GetAxis("Horizontal");
 		//右入力
 		if(horizontalKey > 0){
 			rb.velocity = new Vector2(speed, rb.velocity.y);
@@ -25,6 +31,40 @@ public class player_move : MonoBehaviour{
 		//無入力
 		else{
 			rb.velocity = Vector2.zero;
+		}
+	}
+
+	//接地判定
+	private bool IsGround(){
+		if(isGroundEnter = true || isGroundStay == true){
+			isGround = true;
+		}else if(isGroundExit == true){
+			isGround = false;
+		}
+		//初期化
+		isGroundEnter = false;
+		isGroundStay = false;
+		isGroundExit = false;
+		//返り値設定
+		return isGround;
+	}
+	
+	//接地時
+	private void OnTriggerEnter2D( Collider2D other) {
+		if(other.tag == "Ground"){
+			isGroundEnter = true;
+		}
+	}
+	//接地中
+	private void OnTriggerStay2D( Collider2D other) {
+		if(other.tag == "Ground"){
+			isGroundStay = true;
+		}
+	}
+	//接地終了時
+	private void OnTriggerExit2D( Collider2D other) {
+		if(other.tag == "Ground"){
+			isGroundExit = true;
 		}
 	}
 }
