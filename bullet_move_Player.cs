9d8d32e7c33869d0df4b9b98d76a31cd272f093e
mekatama <tamatte@hotmail.com>
2021-03-09 23:01:34 +0900

    12 bullet作る

---
+++ bullet_move_Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_move_Player : MonoBehaviour{
+	public float speed = 0.0f;	//弾の移動速度
+	void Start(){
+	}
+
+	void Update(){
+		//移動
+		transform.Translate(Vector3.right * speed * Time.deltaTime);
+	}
+}
