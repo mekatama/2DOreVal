bcf6c61b0a20f895de724ddb34828193f49d5535
mekatama <tamatte@hotmail.com>
2021-04-04 19:03:56 +0900

    14shot方向変更

---
+++ player_shot_roll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_shot_roll : MonoBehaviour{
+
+	void Start(){
+	}
+
+	void Update(){
+		//キー入力判定
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, 90f); 
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, -90f); 
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, 0); 
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, 180f); 
+		}
+		if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, 45f); 
+		}
+		if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, 135f); 
+		}
+		if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, -45f); 
+		}
+		if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)) {
+			transform.rotation = Quaternion.Euler(0, 0, -135f); 
+		}
+	}
+}
