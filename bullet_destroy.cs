363ef2331328eb54c1e777af41e7b56e630a029f
mekatama <tamatte@hotmail.com>
2021-04-11 01:20:21 +0900

    15bullet destroy

---
+++ bullet_destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_destroy : MonoBehaviour{
+
+	void Start(){
+		//生成から5秒で削除
+		Destroy(gameObject,1.0f);
+	}
+
+    void Update(){
+
+	}
+
+	//他のオブジェクトとの当たり判定(trigger)
+	void OnTriggerEnter2D( Collider2D other) {
+		if(other.tag == "Ground"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+		if(other.tag == "Wall"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+	}
+}
