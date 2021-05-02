5b21fc8899644aa0b0c73ce0f575ea9ff73d84c5
mekatama <tamatte@hotmail.com>
2021-05-02 17:53:32 +0900

    17 jump dash motion add

--- player_animation.cs
+++ player_animation.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class player_animation : MonoBehaviour{
 	private Vector3 velocity;	//判定用のベクトル入れる用
 	private Animator animator;	//Animator入れる用
+	private player_groundCheck groundCheck;	//player_groundCheckを参照
+	private player_move playerMove;			//player_moveを参照
 
 	void Start(){
 		animator = GetComponent<Animator>();	//Animator取得
+		//子オブジェクトのplayer_groundCheck.cs参照
+		groundCheck = GetComponentInChildren<player_groundCheck>();
+		//同じオブジェクトのplayer_move.cs参照
+		playerMove = GetComponent<player_move>();
 	}
 
 	void Update(){
@@ -17,10 +23,28 @@ public class player_animation : MonoBehaviour{
 			//walk motion
 			animator.SetFloat("speed",velocity.magnitude);
 			//入力方向を向く
-//			transform.LookAt(transform.position + velocity);
+			transform.LookAt(transform.position + velocity);
 		}else{
 			//idol motion
 			animator.SetFloat("speed",0f);
+		}
+
+		//jump motion制御
+		if(groundCheck.isGround == false){
+			//jump motion
+			animator.SetBool("jump",true);
+		}else{
+			//idol motion
+			animator.SetBool("jump",false);
+		}
+
+		//dash moyion制御
+		if(playerMove.isDash == true){
+			//dash motion
+			animator.SetBool("dash",true);
+		}else{
+			//idol motion
+			animator.SetBool("dash",false);
 		}
 	}
 }
