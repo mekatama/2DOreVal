c52c38cbf2f57b3f9c5d19bd7dc8d41e35301703
mekatama <tamatte@hotmail.com>
2021-05-06 00:05:35 +0900

    20 dash effect

---
+++ effect_dash1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect_dash1 : MonoBehaviour{
+	public ParticleSystem particle;	//particle入れる用
+	private player_move playerMove;	//player_move.csを参照
+	private bool goParticle;
+
+    void Start(){
+		goParticle = false;	//初期化
+		//同じオブジェクトのParticleSystemを参照
+		particle = this.GetComponent<ParticleSystem>();
+		//親オブジェクトのplayer_move.cs参照
+		playerMove = GetComponentInParent<player_move>();
+	}
+
+    void Update(){
+		//ダッシュ時のみ再生したい
+		if(playerMove.isDash == true && goParticle == false){
+			particle.Play();
+			goParticle = true;
+		}
+		if(playerMove.isDash == false && goParticle == true){
+			particle.Stop();
+			goParticle = false;
+		}
+	}
+}
