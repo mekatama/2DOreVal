3c373857f89eeb4496fac6dc02a0156d1db889bf
mekatama <tamatte@hotmail.com>
2021-05-08 19:03:04 +0900

    23 damege 点滅演出

--- enemy1.cs
+++ enemy1.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemy1 : MonoBehaviour{
-	public int enemyHp;		//EnemyのHP
-	private bool isDeth;	//死亡flag
+	public int enemyHp;				//EnemyのHP
+	private bool isDeth;			//死亡flag
+	private bool isDamage;			//damage flag
+	public SpriteRenderer s;	//
 
     void Start(){
 		isDeth = false;		//初期化
+		isDamage = false;	//初期化
+		s = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+    void Update(){
+		//ダメージ点滅処理
+		if(isDamage == true){
+			float level = Mathf.Abs(Mathf.Sin(Time.time * 10));
+			s.color = new Color(1f, 0f, 0f, level);	//赤点滅
+		}
 	}
 
 	//他のオブジェクトとの当たり判定(trigger)
@@ -19,6 +31,8 @@ public class enemy1 : MonoBehaviour{
 				if(enemyHp <= 0){
 					enemyHp = 0;
 				}
+				isDamage = true;
+				StartCoroutine(onDamage());
 			}
 			//死亡判定
 			if(enemyHp == 0){
@@ -30,4 +44,12 @@ public class enemy1 : MonoBehaviour{
 		}
 	}
 
+	//点滅制御用コルーチン
+	public IEnumerator onDamage(){
+		//指定時間待機
+		yield return new WaitForSeconds(0.1f);
+		//通常に戻す
+		isDamage = false;
+		s.color = new Color(1f, 1f, 1f, 1f);	
+	}
 }
