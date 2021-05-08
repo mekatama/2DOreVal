815092e73a3455f38b9896b5d46ae3b538332f5f
mekatama <tamatte@hotmail.com>
2021-05-08 16:27:29 +0900

    22 hiteffect 追加

--- bullet_destroy.cs
+++ bullet_destroy.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bullet_destroy : MonoBehaviour{
+	public GameObject hitEffect;	//HitEffectアニメのプレハブを入れる用
 
 	void Start(){
 		//生成から5秒で削除
 		Destroy(gameObject,2.0f);
 	}
 
-    void Update(){
-
-	}
-
 	//他のオブジェクトとの当たり判定(trigger)
 	void OnTriggerEnter2D( Collider2D other) {
 		if(other.tag == "Ground"){
 			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+			Instantiate (hitEffect, transform.position, transform.rotation);
 		};
 		if(other.tag == "Wall"){
 			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+			Instantiate (hitEffect, transform.position, transform.rotation);
 		}
 		if(other.tag == "Enemy"){
 			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+			Instantiate (hitEffect, transform.position, transform.rotation);
 		}
 	}
 }
---
+++ effect_destroy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect_destroy : MonoBehaviour{
+
+	public void destroy(){
+		Destroy(gameObject);	//オブジェクトの削除
+	}
+}
