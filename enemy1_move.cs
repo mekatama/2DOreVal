3f69209ad866561efad2702c77400fba6c64b341
mekatama <tamatte@hotmail.com>
2021-05-12 23:35:01 +0900

    27 enemy right move

---
+++ enemy1_move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1_move : MonoBehaviour{
+	public float speed;				//移動速度
+	private Rigidbody2D rb = null;	//Rigidbody2D入れる用
+
+    void Start(){
+		rb = GetComponent<Rigidbody2D>();	//Rigidbody2D取得
+	}
+
+	//物理演算用
+	void FixedUpdate(){
+		//実際に移動
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+	}
+
+
+    void Update(){
+	}
+}
