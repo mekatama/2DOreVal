3ec0ecf99cc1f98cb6392356b229c477b0bbedf7
mekatama <tamatte@hotmail.com>
2021-05-14 00:06:05 +0900

    28 enemy反転させたい

---
+++ enemy1_direction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1_direction : MonoBehaviour{
+	public bool isForward;	//前移動flag
+
+	void Start(){
+		isForward = true;	//初期化
+	}
+
+	//他のオブジェクトとの当たり判定(collision))
+	void OnCollisionEnter2D(Collision2D other) {
+		if(other.gameObject.tag == "Wall"){
+			if(isForward == true){
+				isForward = false;
+			}else{
+				isForward = true;
+			}
+		}
+	}
+}
--- enemy1_move.cs
+++ enemy1_move.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class enemy1_move : MonoBehaviour{
 	public float speed;				//移動速度
 	private Rigidbody2D rb = null;	//Rigidbody2D入れる用
+	private enemy1_direction childScript;	//enemy1_direction.csスクリプト入れる用
 
     void Start(){
 		rb = GetComponent<Rigidbody2D>();	//Rigidbody2D取得
+		childScript = GetComponentInChildren<enemy1_direction>();
 	}
 
 	//物理演算用
 	void FixedUpdate(){
 		//実際に移動
-		rb.velocity = new Vector2(speed, rb.velocity.y);
+		if(childScript.isForward == true){
+			//前進
+			rb.velocity = new Vector2(speed, rb.velocity.y);
+		}else{
+			//後進
+			rb.velocity = new Vector2(speed * -1, rb.velocity.y);
+		}
 	}
 
 
