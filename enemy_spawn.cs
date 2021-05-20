a8f326d03c179f59ecccb75fa804c93690bd2ec5
mekatama <tamatte@hotmail.com>
2021-05-20 22:55:04 +0900

    35 spawns

--- enemy_spawn.cs
+++ enemy_spawn.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemy_spawn : MonoBehaviour{
-	public GameObject[] enemyObject;		//enemyのプレハブを配列で管理
-	public Transform[] spawnPosition;		//enemyの生成位置を配列で管理
+	public GameObject[] enemyObject;	//enemyのプレハブを配列で管理
+	public Transform[] spawnPosition;	//enemyの生成位置を配列で管理
 	public GameObject enemy;			//実際に生成されるenemy
-	private int enemyType;					//enemyの種類
-	private int spawnType;					//spawnの場所
-	private float timeElapsed = 0.0f;		//生成間隔カウント用
-	public float timeOut;					//生成間隔の時間
+	private int enemyType;				//enemyの種類
+	private int spawnType;				//spawnの場所
+	private float timeElapsed = 0.0f;	//生成間隔カウント用
+	public float timeOut;				//生成間隔の時間
+	public bool isSpawn;				//enemy生成flag
 
     void Start(){
 		enemyType = 0;	//初期化
 		spawnType = 0;	//初期化
-		enemy = null;
+		enemy = null;	//
+		isSpawn = true;	//初期化
 	}
 
     void Update(){
-		enemySpawn();
+		if(isSpawn == true){
+			enemySpawn();
+		}
 	}
+
 	void enemySpawn(){
 		enemyType = 0;					//敵の種類設定、仮で0固定
-		spawnType = Random.Range(0,2);	//生成位置設定
+		spawnType = Random.Range(0,5);	//生成位置設定
 		timeElapsed += Time.deltaTime;	//カウント
 		if(timeElapsed >= timeOut){
 			//enemyの生成位置を指定
@@ -38,9 +43,23 @@ public class enemy_spawn : MonoBehaviour{
 			//生成時の移動方向を指定
 			switch(spawnType){
 				case 0:
-					s.isForward = false;	//左に移動
+					int temp = Random.Range(0,2);
+					if(temp == 0){
+						s.isForward = false;	//左に移動
+					}else{
+						s.isForward = true;		//右に移動
+					}
 					break;
 				case 1:
+					s.isForward = false;	//左に移動
+					break;
+				case 2:
+					s.isForward = true;		//右に移動
+					break;
+				case 3:
+					s.isForward = false;	//左に移動
+					break;
+				case 4:
 					s.isForward = true;		//右に移動
 					break;
 			}
