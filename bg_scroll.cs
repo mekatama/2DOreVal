038f931fc2a2ecb915b5b2f98cacb394507bc2fb
mekatama <tamatte@hotmail.com>
2021-05-30 22:39:46 +0900

    42 BG scroll

---
+++ bg_scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bg_scroll : MonoBehaviour{
+	private GameObject player;
+	private Vector3 startPlayerOffset;
+	private Vector3 startCameraPos;
+	private static readonly float RATE = 0.3f;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+		startPlayerOffset = player.transform.position;
+		startCameraPos = this.transform.position;
+	}
+
+	void Update() {
+		Vector3 v = (player.transform.position - startPlayerOffset) * RATE;
+		this.transform.position = new Vector3 (startCameraPos.x + v.x, transform.position.y, transform.position.z);
+	}
+}
