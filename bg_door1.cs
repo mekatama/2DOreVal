8c0da81e2f1255af6160015c85f7b89d4e6ac0b6
mekatama <tamatte@hotmail.com>
2021-06-13 23:11:46 +0900

    49 door

---
+++ bg_door1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bg_door1 : MonoBehaviour{
+	private Animator animator;	//Animator入れる用
+	private bool isOpen;		//flag
+	public BoxCollider2D col2D;//BoxCollider2D入れる用
+
+	void Start(){
+		animator = GetComponent<Animator>();	//Animator取得
+//		col2D = GetComponent<BoxCollider2D>();	//BoxCollider2D取得
+	}
+
+	void Update(){
+	}
+	//他のオブジェクトとの当たり判定(trigger))
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player"){
+			Debug.Log("open");
+			//door制御
+			if(isOpen == false){
+				//open motion
+				animator.SetBool("isOpen",true);
+				//collider off
+				col2D.enabled = false;
+				isOpen = true;
+			}
+		}
+	}
+	//他のオブジェクトとの当たり判定(trigger))
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.tag == "Player"){
+			Debug.Log("close");
+			//door制御
+			if(isOpen == true){
+				//close motion
+				animator.SetBool("isOpen",false);
+				//collider on
+				col2D.enabled = true;
+				isOpen = false;
+			}
+		}
+	}
+
+}
