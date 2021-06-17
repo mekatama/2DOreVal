951cbc5ab314fc5fecbd0f1381a5affeb786be5c
mekatama <tamatte@hotmail.com>
2021-06-17 22:58:46 +0900

    50 enemy

--- enemy1_move.cs
+++ enemy1_move.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 public class enemy1_move : MonoBehaviour{
 	public float speed;				//移動速度
 	private Rigidbody2D rb = null;	//Rigidbody2D入れる用
-	private enemy1_direction childScript;	//enemy1_direction.csスクリプト入れる用
-	private enemy_radar1 childScript2;		//enemy_rader1.csスクリプト入れる用
-	public GameObject child;				//enemy_rader1.csをアッタチしているオブジェクト用
+
+	public GameObject objModel;				//enemy1_direction.csをアッタチしているオブジェクト用
+	private enemy1_direction scrDirection;	//enemy1_direction.csスクリプト入れる用
+	public GameObject objRader;				//enemy_rader1.csをアッタチしているオブジェクト用
+	private enemy_radar1 scrRader1;			//enemy_rader1.csスクリプト入れる用
+
+	//動き種類select
+	public enum MoveType{
+		stop = 0,
+		advance = 1,
+		pause = 2
+	}
+	public MoveType moveType;
 
     void Start(){
 		rb = GetComponent<Rigidbody2D>();	//Rigidbody2D取得
-		childScript = GetComponentInChildren<enemy1_direction>();
-		childScript2 = child.GetComponent<enemy_radar1>();
+		scrDirection = objModel.GetComponent<enemy1_direction>();
+		scrRader1 = objRader.GetComponent<enemy_radar1>();
 	}
 
 	//物理演算用
 	void FixedUpdate(){
-		//player発見
-		if(childScript2.isDiscovery == true){
-			//停止
-			rb.velocity = Vector2.zero;
-
-		}else{
+		//前進するだけ
+		if((int)moveType == 1){
 			//実際に移動
-			if(childScript.isForward == true){
+			if(scrDirection.isForward == true){
 				//前進
 				rb.velocity = new Vector2(speed, rb.velocity.y);
 			}else{
@@ -32,9 +38,22 @@ public class enemy1_move : MonoBehaviour{
 				rb.velocity = new Vector2(speed * -1, rb.velocity.y);
 			}
 		}
-	}
-
-
-    void Update(){
+		//レーダー使用で前進
+		if((int)moveType == 2){
+			//player発見
+			if(scrRader1.isDiscovery == true){
+				//停止
+				rb.velocity = Vector2.zero;
+			}else{
+				//実際に移動
+				if(scrDirection.isForward == true){
+					//前進
+					rb.velocity = new Vector2(speed, rb.velocity.y);
+				}else{
+					//後進
+					rb.velocity = new Vector2(speed * -1, rb.velocity.y);
+				}
+			}
+		}
 	}
 }
--- enemy_shot1.cs
+++ enemy_shot1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemy_shot1 : MonoBehaviour{
-	public GameObject bulletObject = null;		//弾のプレハブ
+	public GameObject[] bulletObject = null;	//弾のプレハブ 配列
 	public GameObject effectObject = null;		//マズルエフェクトのプレハブ
 	public Transform bulletStartPosition = null;//弾の発射位置
 	private float timeElapsed = 0.0f;			//弾の連射間隔カウント用
@@ -13,14 +13,21 @@ public class enemy_shot1 : MonoBehaviour{
 	private int numShotCount;					//1setの攻撃数のカウント用
 	public float stopTime;						//一時停止時間
 
-	public GameObject parent;				//enemy1_direction.csをアッタチしているオブジェクト用
-	private enemy1_direction childScript;	//enemy1_direction.csスクリプト入れる用
-	public GameObject parent2;				//enemy_rader1.csをアッタチしているオブジェクト用
-	private enemy_radar1 childScript2;		//enemy_rader1.csスクリプト入れる用
+	public GameObject objModel;				//enemy1_direction.csをアッタチしているオブジェクト用
+	private enemy1_direction scrDirection;	//enemy1_direction.csスクリプト入れる用
+	public GameObject objRader;				//enemy_rader1.csをアッタチしているオブジェクト用
+	private enemy_radar1 scrRader1;			//enemy_rader1.csスクリプト入れる用
+
+	//弾の種類select
+	public enum BulletType{
+		straight = 0,
+		howitzer = 1
+	}
+	public BulletType bulletType;
 
 	void Start(){
-		childScript = parent.GetComponent<enemy1_direction>();
-		childScript2 = parent2.GetComponent<enemy_radar1>();
+		scrDirection = objModel.GetComponent<enemy1_direction>();
+		scrRader1 = objRader.GetComponent<enemy_radar1>();
 		numShotCount = 0;	//初期化
 	}
 
@@ -28,7 +35,7 @@ public class enemy_shot1 : MonoBehaviour{
 		//Debug用flag
 		if(isShot == true){
 			//player発見したら
-			if(childScript2.isDiscovery == true){
+			if(scrRader1.isDiscovery == true){
 				//1set攻撃回数判定
 				if(numShotCount < numShot){
 					Shot();
@@ -56,13 +63,16 @@ public class enemy_shot1 : MonoBehaviour{
 			Vector3 vecBulletPos = bulletStartPosition.position;
 			//弾を生成
 			GameObject shot = Instantiate(
-						bulletObject,
+						bulletObject[(int)bulletType],
 						vecBulletPos,
 						transform.rotation
 						);
-			//弾に	enemyの向きを伝える
-			bullet_move_Enemy1 flag = shot.GetComponent<bullet_move_Enemy1>();
-			flag.isRight = childScript.isForward;
+			//榴弾howitzerの時だけ
+			if((int)bulletType == 1){
+				//弾に	enemyの向きを伝える
+				bullet_move_Enemy1 flag = shot.GetComponent<bullet_move_Enemy1>();
+				flag.isRight = scrDirection.isForward;
+			}
 
 			//マズルエフェクトを生成
 			Instantiate(
